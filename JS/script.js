f60211b99fd7ac73d06a29c36ffaf7df1dd39a00
harish <harishvithanala04@gmail.com>
2023-07-25 11:41:49 +0530

    JS modified

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,8 @@
 let name=document.querySelector("#btn-name");
-console.log(name);
+let mblNo= document.querySelector("#btn-mbl");
+let tankerNo= document.querySelector("#btn-tanker");
+let email= document.querySelector("#btn-email");
+
+name.addEventListener("input",()=>{
+    console.log(name.value);
+})
